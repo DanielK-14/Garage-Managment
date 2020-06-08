964c4ae6be160a1c1b6bf89ac091eb0f82a4dd58
DanielK-14 <dany,kalmikov@gmail.com>
2020-06-09 01:28:02 +0300

    Revert "new"
    This reverts commit e38a862d09bb0e419fa7d56fb157a52a0c94ff68, reversing
    changes made to dc223345326c7b465cf9a06d88df130b731083d1.

---
+++ Ex03.GarageLogic/Car.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ex03.GarageLogic
+{
+    class Car : Vehicle
+    {
+        public enum eCarColor
+        {
+            Red = 1,
+            White,
+            Black,
+            Silver
+        }
+
+        public enum eDoorsAmount
+        {
+            NotSigned,
+            TwoDoors,
+            ThreeDoors,
+            FourDoors,
+            FiveDoors
+        }
+
+        private eCarColor? m_CarColor;
+        private eDoorsAmount? m_DoorsAmount;
+
+        public Car(string i_LicenseNumber, Engine i_Engine)
+            : base(i_LicenseNumber, i_Engine, Vehicle.CreateWheelsForVehicle(4, 32)) { }
+
+        public eCarColor CarColor
+        {
+            get
+            {
+                if (m_CarColor.HasValue == true)
+                {
+                    return m_CarColor.Value;
+                }
+                else
+                {
+                    throw new FormatException("Value was not yet initialzed");
+                }
+            }
+            set
+            {
+                if (Enum.IsDefined(typeof(eCarColor), value) == false)
+                {
+                    throw new ArgumentException("Color picked not valid");
+                }
+                else
+                {
+                    m_CarColor = value;
+                }
+            }
+        }
+
+        public eDoorsAmount DoorsAmount
+        {
+            get
+            {
+                if (m_DoorsAmount.HasValue == true)
+                {
+                    return m_DoorsAmount.Value;
+                }
+                else
+                {
+                    throw new FormatException("Value was not yet initialzed");
+                }
+            }
+            set
+            {
+                if (Enum.IsDefined(typeof(eDoorsAmount), value) == false)
+                {
+                    throw new ArgumentException("Doors Amount picked not valid");
+                }
+                else
+                {
+                    m_DoorsAmount = value;
+                }
+            }
+        }
+
+        public override List<string> RequiredInfoForCreation()
+        {
+            List<string> requiredInfo = base.RequiredInfoForCreation();
+            requiredInfo.Add("Please choose COLOR:\n" + Garage.GetEnumOptions(typeof(eCarColor)));
+            requiredInfo.Add("Please choose DOORS AMOUNT:\n" + Garage.GetEnumOptions(typeof(eDoorsAmount)));
+
+            return requiredInfo;
+        }
+        public override StringBuilder ShowInfo()
+        {
+            StringBuilder vehicleInfo = base.ShowInfo();
+            
+            vehicleInfo.AppendLine("Car color: " + m_CarColor.ToString());
+            vehicleInfo.AppendLine("Doors amount: " + m_DoorsAmount.ToString());
+
+            return vehicleInfo;
+        }
+    }
+}
---
+++ Ex03.GarageLogic/Motorcycle.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace Ex03.GarageLogic
+{
+    class Motorcycle : Vehicle
+    {
+        public enum eLicenseType
+        {
+            A = 1,
+            A1,
+            AA,
+            B
+        }
+
+        private eLicenseType? m_LicenseType;
+        private int? m_EngineCapacity;
+
+        public Motorcycle(string i_LicenseNumber, Engine i_Engine)
+            : base(i_LicenseNumber, i_Engine, Vehicle.CreateWheelsForVehicle(2, 30)) { }
+
+        public eLicenseType LicenseType
+        {
+            get
+            {
+                if (m_LicenseType.HasValue == true)
+                {
+                    return m_LicenseType.Value;
+                }
+                else
+                {
+                    throw new FormatException("Value was not yet initialzed");
+                }
+            }
+            set
+            {
+                if (Enum.IsDefined(typeof(eLicenseType), value) == false)
+                {
+                    throw new ArgumentException("License type picked not valid");
+                }
+                else
+                {
+                    m_LicenseType = value;
+                }
+            }
+        }
+
+        public int EngineCapacity
+        {
+            get
+            {
+                if (m_EngineCapacity.HasValue == true)
+                {
+                    return m_EngineCapacity.Value;
+                }
+                else
+                {
+                    throw new FormatException("Value was not yet initialzed");
+                }
+            }
+            set
+            {
+                m_EngineCapacity = value;
+            }
+        }
+
+        public override List<string> RequiredInfoForCreation()
+        {
+            List<string> requiredInfo = base.RequiredInfoForCreation();
+            requiredInfo.Add("Please choose LICENSE TYPE: " + Environment.NewLine + Garage.GetEnumOptions(typeof(eLicenseType)));
+            requiredInfo.Add("Please enter ENGINE CPACITY:");
+
+            return requiredInfo;
+        }
+        public override StringBuilder ShowInfo()
+        {
+            StringBuilder vehicleInfo = base.ShowInfo();
+            vehicleInfo.AppendLine("License type: " + Enum.GetName(typeof(eLicenseType), m_LicenseType));
+            vehicleInfo.AppendLine("Engine capacity: " + m_EngineCapacity.ToString());
+
+            return vehicleInfo;
+        }
+    }
+}
